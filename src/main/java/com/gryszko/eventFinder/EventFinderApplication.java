abe82552296e2ceef2db4b6b93f463cbaab734a1
Szymon Gryszko <gryszkoszymon@gmail.com>
2020-11-14 13:14:49 +0100

    Initial commit

---
+++ src/main/java/com/gryszko/eventFinder/EventFinderApplication.java
@@ -0,0 +1,13 @@
+package com.gryszko.eventFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFinderApplication.class, args);
+	}
+
+}
