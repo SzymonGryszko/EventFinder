a9acfc836894c8ed08e19539567233a89420f40b
Szymon Gryszko <gryszkoszymon@gmail.com>
2020-12-03 23:22:11 +0100

    database naming change

--- src/main/java/com/gryszko/eventFinder/model/User.java
+++ src/main/java/com/gryszko/eventFinder/model/User.java
@@ -19,7 +19,7 @@ import static javax.persistence.GenerationType.*;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "eventfinder_users")
+@Table(name = "eventfinder_usr")
 public class User {
 
     @Id
