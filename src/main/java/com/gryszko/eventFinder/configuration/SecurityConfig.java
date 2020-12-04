da511ea5f9cd07d9bb95d264d52cb07e249cd804
Szymon Gryszko <gryszkoszymon@gmail.com>
2020-12-04 15:19:48 +0100

    antMatchers update

--- src/main/java/com/gryszko/eventFinder/configuration/SecurityConfig.java
+++ src/main/java/com/gryszko/eventFinder/configuration/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/admin/**").hasRole(ADMIN.toString())
